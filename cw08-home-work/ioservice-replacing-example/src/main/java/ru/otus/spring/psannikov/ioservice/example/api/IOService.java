9e7f508602dab75935ef7b7546eb72b90b0c7a2c
Pavel Sannikov <psannikov87@gmail.com>
2024-05-05 22:54:35 +0400

    first commit / finish

---
+++ cw08-home-work/ioservice-replacing-example/src/main/java/ru/otus/spring/psannikov/ioservice/example/api/IOService.java
@@ -0,0 +1,6 @@
+package ru.otus.spring.psannikov.ioservice.example.api;
+
+public interface IOService {
+    void out(String message);
+    String readString();
+}
---
+++ cw08-home-work/unit-testing-pure-spring-parallel/src/main/java/ru/otus/spring/psannikov/testing/example/services/IOService.java
@@ -0,0 +1,6 @@
+package ru.otus.spring.psannikov.testing.example.services;
+
+public interface IOService {
+    void out(String message);
+    String readString();
+}
