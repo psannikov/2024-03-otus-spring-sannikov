942173729a0ef6842e24c823e2e7413475442186
psannikov <p.sannikov@volga.rt.ru>
2024-05-13 13:23:27 +0400

    CsvQuestionDaoTest

--- hw04-spring-boot-shell/src/test/java/ru/otus/spring/psannikov/spring/boot/shell/dao/CsvQuestionDaoTest.java
+++ hw04-spring-boot-shell/src/test/java/ru/otus/spring/psannikov/spring/boot/shell/dao/CsvQuestionDaoTest.java
@@ -2,6 +2,7 @@ package ru.otus.spring.psannikov.spring.boot.shell.dao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.spring.psannikov.spring.boot.shell.config.AppProperties;
@@ -13,14 +14,19 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest(properties = "spring.shell.interactive.enabled=false")
 public class CsvQuestionDaoTest {
-    @MockBean
     private AppProperties appProperties;
-    @MockBean
     private TestFileNameProvider fileNameProvider;
     private CsvQuestionDao csvQuestionDao;
 
     final static String QUESTIONS_FILE_NAME_TEST = "questions.csv";
     final static String QUESTION = "How many letters are in the Russian alphabet?";
+
+    @BeforeEach
+    void init() {
+        appProperties = mock(AppProperties.class);
+        fileNameProvider = mock(TestFileNameProvider.class);
+        csvQuestionDao = new CsvQuestionDao(appProperties, fileNameProvider);
+    }
 
     @Test
     void testGetFileFromResourceAsStream ()
@@ -32,7 +38,6 @@ public class CsvQuestionDaoTest {
     void testFindAll() {
         when (fileNameProvider.getTestFileName()).thenReturn(QUESTIONS_FILE_NAME_TEST);
         var listQuestions = csvQuestionDao.findAll();
-        System.out.println(listQuestions.size());
-//        assertEquals(QUESTION, listQuestions.getFirst().text());
+        assertEquals(QUESTION, listQuestions.get(0).text());
     }
 }
