9a6c237cc2092437121b0ebf7b1962ced08c5e88
psannikov <p.sannikov@volga.rt.ru>
2024-06-24 12:05:20 +0400

    modif

--- cw20-reactor/src/main/java/ru/otus/spring/psannikov/reactor/OperatorsExample.java
+++ cw20-reactor/src/main/java/ru/otus/spring/psannikov/reactor/OperatorsExample.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import ru.otus.Person;
+import ru.otus.spring.psannikov.reactor.Person;
 
 
 public class OperatorsExample {
